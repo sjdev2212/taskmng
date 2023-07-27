1aea02dab1e8dd227b2f9243350fe0d75b931d5a
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-07-27 15:17:28 -0300

    add  responsive login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 import "../styles/Navbar.css";
 
-
-
-
-
-const Navbar = ({ isLogged , isAuthenticated , handleLoginOut }) => {
+const Navbar = ({ isLogged, isAuthenticated, handleLoginOut }) => {
   const navigate = useNavigate();
 
-  const loggedOut = () => toast('Until next time', {
-    duration: 3000,
-    position: 'top-center',
-  
-    // Styling
-   
-    className: 'toast-logout',
-  
-    // Custom Icon
-    icon: '👏',
-  
-
-  
- 
-  });
-
-    
-    
-      
-      
-
-  
+  const loggedOut = () =>
+    toast("Until next time", {
+      duration: 3000,
+      position: "top-center",
+      style: {
+        background: "#3450A1",
+        color: "#DA43F0",
+        height: "10vh",
+        width: "35vh",
+        fontSize: "1.2rem",
+        fontWeight: "bold",
+        borderRadius: "15px",
+      },
+      icon: "👏",
+    });
   const handleLogout = () => {
-      localStorage.removeItem("token");
-      handleLoginOut();
-      loggedOut();
-      navigate("/login"); 
+    localStorage.removeItem("token");
+    handleLoginOut();
+    loggedOut();
+    navigate("/login");
   };
-
 
   return (
     <>
@@ -49,21 +37,20 @@ const Navbar = ({ isLogged , isAuthenticated , handleLoginOut }) => {
         </div>
 
         <ul className="ul-btns">
-          { isAuthenticated() ? (
+          {isAuthenticated() ? (
             <>
               <li className="li-btns">
                 <button onClick={handleLogout} className="btns">
-                 Log out
+                  Log out
                 </button>
               </li>
             </>
           ) : (
             <>
               <li className="li-btns">
-                <Link to="/login" className="btns"> 
+                <Link to="/login" className="btns">
                   Login
                 </Link>
-              
               </li>
               <li className="li-btns">
                 <Link to="/register" className="btns">
