f67ac6bff326e4c2381ed19b67b0ef5f9dd17ce9
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-08-01 11:09:57 -0300

    fix login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,51 +1,16 @@
 import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import toast from 'react-hot-toast';
-import axios from "axios";
+
 
 import '../styles/Login.css'
 
-const Login = ({ setIsLogged, onLogin, language }) => {
-  const [email, setEmail] = useState("");
-  const [password, setpassword] = useState("");
+const Login = ({ setIsLogged,
+   onLogin,
+    language, 
+    handleSubmit,
+  setEmail,
+setpassword}) => {
 
-
-  const navigate = useNavigate();
-
-  const logged = () => toast(  'You are logged in', {
-    duration: 3000,
-    position: 'top-center',
-    style: {
-      background: "#3450A1",
-      color: "#DA43F0",
-      height: "10vh",
-      width: "35vh",
-      fontSize: "1.2rem",
-      fontWeight: "bold",
-      borderRadius: "15px",
-    },
-  icon: '👏',
-});
-const logueado = () => toast(  'Estas logueado', {
-  duration: 3000,
-  position: 'top-center',
-  style: {
-    background: "#3450A1",
-    color: "#DA43F0",
-    height: "10vh",
-    width: "35vh",
-    fontSize: "1.2rem",
-    fontWeight: "bold",
-    borderRadius: "15px",
-  },
-icon: '👏',
-});
-
-
-
-
-  const handleEmailChange = (event) => {
+const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
 
@@ -53,26 +18,7 @@ icon: '👏',
     setpassword(event.target.value);
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post(
-        "https://todo-danielamoroso31.b4a.run/login",
-        {
-          email,
-          password,
-        }
-      );
-      const { token } = response.data;
-      localStorage.setItem("token", token);
 
-      onLogin(token);
-      language === 'english' ? logged() : logueado()
-      navigate("/");
-    } catch (error) {
-      console.error(error);
-    }
-  };
   return (
   <main className="main-login">
     <section className="container-login" >
