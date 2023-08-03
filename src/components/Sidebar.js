6fb0e086e4edcab676602dcd04a57e841c16d242
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-08-03 09:41:22 -0300

    create sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/Sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <>
+        <div className="sidebar">
+            <ul>
+                <li>Home</li>
+                <li>Tasks</li>
+                <li>Activities</li>
+                <li>Calendar</li>
+                <li>Settings</li>
+            </ul>
+            
+        </div>
+        
+    </>
+  )
+}
+
+export default Sidebar
