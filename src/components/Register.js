729e9056713d5ee8ff9946dae72d1022cdb90a50
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-08-11 23:16:39 -0300

    fix registraion

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,79 +1,88 @@
 import React from "react";
 import { useState } from "react";
-import  {useNavigate} from "react-router-dom";
-import toast from 'react-hot-toast';
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 import axios from "axios";
-import '../styles/Register.css'
+import "../styles/Register.css";
 
-function Register({language}) {
-
+function Register({ language }) {
   const [email, setEmail] = useState("");
   const [password, setpassword] = useState("");
   const [userName, setUserName] = useState("");
 
   const navigate = useNavigate();
-  const registered = () => toast(  'You are now registered. Please Log in', {
-    duration: 3000,
-    position: 'top-center',
-    style: {
-      background: "#3450A1",
-      color: "#DA43F0",
-      height: "10vh",
-      width: "35vh",
-      fontSize: "1.2rem",
-      fontWeight: "bold",
-      borderRadius: "15px",
-    },
-    icon: '✔️',
-});
-const registrado = () => toast(  'Ahora estas registrado. Por favor inicia sesion', {
-  duration: 3000,
-  position: 'top-center',
-  style: {
-    background: "#3450A1",
-    color: "#DA43F0",
-    height: "10vh",
-    width: "35vh",
-    fontSize: "1.2rem",
-    fontWeight: "bold",
+  const registered = () =>
+    toast("You are now registered. Please Log in", {
+      duration: 3000,
+      position: "top-center",
+      style: {
+        background: "#3450A1",
+        color: "#DA43F0",
+        height: "10vh",
+        width: "35vh",
+        fontSize: "1.2rem",
+        fontWeight: "bold",
+        borderRadius: "15px",
+      },
+      icon: "✔️",
+    });
+  const registrado = () =>
+    toast("Ahora estas registrado. Por favor inicia sesion", {
+      duration: 3000,
+      position: "top-center",
+      style: {
+        background: "#3450A1",
+        color: "#DA43F0",
+        height: "10vh",
+        width: "35vh",
+        fontSize: "1.2rem",
+        fontWeight: "bold",
 
-    borderRadius: "15px",
-  },
-icon: '✔️',
-});
+        borderRadius: "15px",
+      },
+      icon: "✔️",
+    });
 
-const userExists = () => toast(  'There is a user already registered with that email. please enter a different one or login', {
-  duration: 3000,
-  position: 'top-center',
-  style: {
-    background: "#A1051D",
-    color: "white",
-    height: "10vh",
-    width: "50vh",
-    fontSize: "1.2rem",
-    fontWeight: "bold",
-    borderRadius: "15px",
-  },
-icon: '⚠️',
-});
-const existeUsuario = () => toast(  'Ya hay una cuenta registrada con ese correo, por favor usa uno diferente o inicia sesion', {
-  duration: 3000,
-  position: 'top-center',
-  style: {
-    background: "#A1051D",
-    color: "white",
-    height: "10vh",
-    width: "50vh",
-    fontSize: "1.1rem",
-    fontWeight: "bold",
-    borderRadius: "15px",
-  },
-  icon: '⚠️',
-});
+  const userExists = () =>
+    toast(
+      "There is a user already registered with that email. please enter a different one or login",
+      {
+        duration: 3000,
+        position: "top-center",
+        style: {
+          background: "#A1051D",
+          color: "white",
+          height: "10vh",
+          width: "50vh",
+          fontSize: "1.2rem",
+          fontWeight: "bold",
+          borderRadius: "15px",
+        },
+        icon: "⚠️",
+      }
+    );
+  const existeUsuario = () =>
+    toast(
+      "Ya hay una cuenta registrada con ese correo, por favor usa uno diferente o inicia sesion",
+      {
+        duration: 3000,
+        position: "top-center",
+        style: {
+          background: "#A1051D",
+          color: "white",
+          height: "10vh",
+          width: "50vh",
+          fontSize: "1.1rem",
+          fontWeight: "bold",
+          borderRadius: "15px",
+        },
+        icon: "⚠️",
+      }
+    );
 
-const handleUserNameChange = (event) => {
-  setUserName(event.target.value);
-};
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+  };
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
@@ -95,13 +104,12 @@ const handleUserNameChange = (event) => {
       .post("https://todo-danielamoroso31.b4a.run/register", data)
       .then((response) => {
         if (response.status === 200) {
-          language === 'english' ? registered() : registrado();
+          language === "english" ? registered() : registrado();
           navigate("/login");
-        } 
+        }
         if (response.status === 201) {
           navigate("/register");
-          language === 'english' ? userExists() : existeUsuario();
-         
+          language === "english" ? userExists() : existeUsuario();
         }
       })
       .catch((error) => {
@@ -109,26 +117,17 @@ const handleUserNameChange = (event) => {
       });
   };
 
-
-      
-
-      
-
-
-
-
-
   return (
     <main className="main-register">
-      <section className="container-register" >
-        <h1 className="register-title"> 
-        {language === 'english' ? 'Register' : 'Registrarse'}
+      <section className="container-register">
+        <h1 className="register-title">
+          {language === "english" ? "Register" : "Registrarse"}
         </h1>
-     
+
         <form className="form-register" onSubmit={handleSubmit}>
-        <div className="register-groups">
+          <div className="register-groups">
             <label htmlFor="username">
-              {language === 'english' ? 'Username' : 'Nombre de usuario'}
+              {language === "english" ? "Username" : "Nombre de usuario"}
             </label>
             <input
               type="text"
@@ -140,7 +139,7 @@ const handleUserNameChange = (event) => {
           </div>
           <div className="register-groups">
             <label htmlFor="email">
-              {language === 'english' ? 'Email' : 'Correo electrónico'}
+              {language === "english" ? "Email" : "Correo electrónico"}
             </label>
             <input
               type="email"
@@ -152,7 +151,7 @@ const handleUserNameChange = (event) => {
           </div>
           <div className="register-groups">
             <label htmlFor="password">
-              {language === 'english' ? 'Password' : 'Contraseña'}
+              {language === "english" ? "Password" : "Contraseña"}
             </label>
             <input
               type="password"
@@ -160,12 +159,12 @@ const handleUserNameChange = (event) => {
               value={password}
               onChange={handlePasswordChange}
               required
-              pattern=".{6,}" 
+              pattern=".{6,}"
               title="Password must be at least 6 characters long."
             />
           </div>
           <button className="register-btn" type="submit">
-            {language === 'english' ? 'Register' : 'Registrarse'}
+            {language === "english" ? "Register" : "Registrarse"}
           </button>
         </form>
       </section>
