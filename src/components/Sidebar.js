f2266853f35a22631176bda6b024db955df25312
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-08-27 01:35:32 -0300

    add func

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,43 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {BsCalendar3} from 'react-icons/bs'
+import {FaTasks} from 'react-icons/fa'
 import '../styles/Sidebar.css'
 
 const Sidebar = ({language, theme}) => {
   const sidebarTheme = theme === 'light' ? 'sidebar-container-light' : 'sidebar-container-dark'
+  const ulTheme = theme === 'light' ? 'ul-theme-light' : 'ul-theme-dark'
+  const calIconTheme = theme === 'light' ? 'cal-icon-light' : 'cal-icon-dark'
+  const taskIconTheme = theme === 'light' ? 'task-icon-light' : 'task-icon-dark'
+
   return (
     <>
         <div className={sidebarTheme}>
-            <ul>
-                <li>Home</li>
-                <li>
-                  <Link to="/tasks" >
+            <ul className={ulTheme}>
+                
+                  <Link className='link' to="/tasks" >
+                <li  >
                   Tasks
-                  </Link>
+                 <div className={taskIconTheme}>
+                    <FaTasks/>
+                    </div>
                   </li>
+                  </Link>
 
-                <li>Activities</li>
-                <li>Calendar</li>
-                <li>Settings</li>
+                  <Link className='link' to="/bills" >
+                <li>
+                  Bills & Payments
+                   </li>
+                  </Link>
+                <Link className='link' to="/calendar" >
+                <li>
+                  Calendar
+                  <div className={calIconTheme}>
+                <BsCalendar3/>
+                  </div>
+                </li>
+                  </Link>
+               
             </ul>
             
         </div>
