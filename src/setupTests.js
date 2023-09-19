0fa30b4b4ad650fa013dee2b870b8354a5a2287f
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-09-19 14:31:48 -0300

    try jest

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+//
+// For more information on customizing Jest test runner configuration, see:
+// https://jestjs.io/docs/configuration
+
