4ef51c41552b28ad69f08a41d9e1665d2554fc6d
sjdev2212 <daniel_amoroso31@hotmail.com>
2023-09-19 14:32:01 -0300

    browserrouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+ <App />
     </BrowserRouter>
+   
+    
   </React.StrictMode>
 );
 
